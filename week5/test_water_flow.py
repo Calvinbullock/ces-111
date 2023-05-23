67de919b0fd05c4a44aa9980a2972edd3282e242
Calvinbullock <calvinnbullock@gamil.com>
2023-05-23 16:04:49 -0600

    formating fixes

--- week5/test_water_flow.py
+++ week5/test_water_flow.py
@@ -44,11 +44,17 @@ def test_pressure_loss_from_pipe():
     """
     assert pressure_loss_from_pipe(0.048692, 0, 0.018, 1.75) == approx(0, 0.001)
     assert pressure_loss_from_pipe(0.048692, 200, 0.0, 1.75) == approx(0, 0.001)
-    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 0)  == approx(0, 0.001)
-    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.75) == approx(-113.008, 0.001)
-    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.65) == approx(-100.462, 0.001)
+    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 0) == approx(0, 0.001)
+    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.75) == approx(
+        -113.008, 0.001
+    )
+    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.65) == approx(
+        -100.462, 0.001
+    )
     assert pressure_loss_from_pipe(0.28687, 1000, 0.013, 1.65) == approx(-61.576, 0.001)
-    assert pressure_loss_from_pipe(0.28687, 1800.75, 0.013, 1.65) == approx(-110.884, 0.001)
+    assert pressure_loss_from_pipe(0.28687, 1800.75, 0.013, 1.65) == approx(
+        -110.884, 0.001
+    )
 
 
 def test_pressure_loss_from_fittings():
@@ -57,7 +63,7 @@ def test_pressure_loss_from_fittings():
     Parameters: none
     Return: nothing
     """
-    assert pressure_loss_from_fittings(0, 3)    == approx(0, 0.001)
+    assert pressure_loss_from_fittings(0, 3) == approx(0, 0.001)
     assert pressure_loss_from_fittings(1.65, 0) == approx(0, 0.001)
     assert pressure_loss_from_fittings(1.65, 2) == approx(-0.109, 0.003)
     assert pressure_loss_from_fittings(1.75, 2) == approx(-0.122, 0.003)
@@ -84,12 +90,14 @@ def test_pressure_loss_from_pipe_reduction():
     Parameters: none
     Return: nothing
     """
-    assert pressure_loss_from_pipe_reduction(0.28687, 0, 1, 0.048692) == approx(0, 0.001)
-    assert (
-        pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(-163.744, 0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 0, 1, 0.048692) == approx(
+        0, 0.001
     )
-    assert (
-        pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(-184.182, 0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(
+        -163.744, 0.001
+    )
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(
+        -184.182, 0.001
     )
 
 
