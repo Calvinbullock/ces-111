3067e3fddb4177f100274eaed4b8ef7342e931c8
Calvinbullock <calvinnbullock@gamil.com>
2023-07-12 09:21:14 -0600

    added comments to test_othello

--- final/test_othello.py
+++ final/test_othello.py
@@ -149,3 +149,9 @@ def test_parse_cords():
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
 pytest.main(["-v", "--tb=line", "-rN", __file__])
+
+
+# Directional math compass
+#   -9        N
+# -1  +1    W   E
+#   +9        S
